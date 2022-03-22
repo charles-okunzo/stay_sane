9654e024d619c418fa5bb441a825ee316b9d2f2f
charles okunzo <okunzopeter01@gmail.com>
2022-03-22 19:53:09 +0300

    feature:hover-enabled overlay toggle on hover

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let divsArr=['kard1', 'kard2', 'kard3', 'kard4', 'kard5', 'kard6', 'kard7', 'kard8'];
+
+divsArr.forEach((div)=>{
+  $(`#${div}`).hover(()=>{
+    $(`#${div}-txt`).slideToggle(1000);
+    $(`#${div}-overlay`).slideToggle(1000)
+  })
+})
