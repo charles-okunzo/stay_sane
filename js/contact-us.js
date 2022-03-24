75874dfcb7db4c3f6af17d70ffddaab12b37df5c
Alvinkenyagah <alvin47k@gmail.com>
2022-03-24 14:04:58 +0300

    update body

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,11 +1,14 @@
 function sendEmail() {
   Email.send({
     Host: "smtp.gmail.com",
-    Username: "alvin.kenyagah@student.moringaschool.com",
+    Username: "alvin47k@gmail.com",
     Password: "MAZARATI111",
-    To: 'alvin47k@gmail.com',
+    To: 'alvin.kenyagah@student.moringaschool.com',
     From: document.getElementById("email").value,
     Subject: "CONTACT US FORM INQUIRY",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
+    Body: "Name: " + document.getElementById("name").value
+    + "<br>Name: " + document.getElementById("name").value
+    + "<br>Name: " + document.getElementById("name").value
+    + "<br>Name: " + document.getElementById("name").value
+  }).then((message) => alert("Message Sent Succesfully"));
 }
