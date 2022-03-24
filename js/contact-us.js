4b0ea7dd41ac8da74c35cbce0c262b130dd7d0a4
Alvinkenyagah <alvin47k@gmail.com>
2022-03-24 13:40:35 +0300

    update to email adress

--- js/contact-us.js
+++ js/contact-us.js
@@ -3,7 +3,7 @@ function sendEmail() {
     Host: "smtp.gmail.com",
     Username: "alvin.kenyagah@student.moringaschool.com",
     Password: "MAZARATI111",
-    To: "alvin47k@gmail.com",
+    To: 'alvin47k@gmail.com',
     From: document.getElementById("email").value,
     Subject: "CONTACT US FORM INQUIRY",
     Body: "And this is the body",
